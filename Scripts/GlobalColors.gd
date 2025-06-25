c46af16a5d5fdf2e78b3b47292f9eb80ff071c9d
Naut <nautastrogaming@gmail.com>
2025-06-25 18:25:10 -0300

    Sessão de Desenvolvimento 1
    - Criação de cena de background para facilitação de uso repetido futuro;
    
    - Criação de cena para configuração de cores do jogo (estético);
    
    - Criação de script de configurações de cor globais, a fim de distribuir a configuração do usuário para o resto do jogo;
    
    -/-TODO-/-
    
    - Implementar de fato as configurações;
    
    - Armazenar as escolhas do usuário mesmo após encerrar a execução;
    
    - Oferecer opção de reset ao padrão;

---
+++ Scripts/GlobalColors.gd
@@ -0,0 +1,7 @@
+extends Node
+
+const stardefaultcolor := Color(1,1,1,1) #RGBA
+const bgdefaultcolor := Color(0,0,0.14,1) #RGBA
+
+var starcolor := Color(1,1,1,1) #RGBA
+var bgcolor := Color(0,0,0.14,1) #RGBA
